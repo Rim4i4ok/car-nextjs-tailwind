98ce16d3cefd4648f0ddf708d11628016bb11ef1
Rim4i4ok <rim4i4ok@gmail.com>
2023-09-10 19:21:07 +0200

    filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+import CustomFilter from "@/components/CustomFilter";
 import Hero from "@/components/Hero";
+import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div id="discovery" className="mt-12 padding-x paxxing-y max-width">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
